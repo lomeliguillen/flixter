2346a50d4df0e2d12eb13bf1333f2d2ba1f8118b
lomeliguillen@gmail.com <lomeliguillen@gmail.com>
2019-09-08 23:07:59 +0000

    lesson 29

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,11 +1,21 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_enrolled_in_current_course, only: [:show]
+
   def show
   end
 
   private
 
+  def require_enrolled_in_current_course
+    if !current_user.enrolled_in?(current_lesson.section.course)
+      redirect_to course_path(current_lesson.section.course), alert: 'You are not enrolled'
+    end
+  end
+
   helper_method :current_lesson
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
   end
+
 end

3c96c9609fc4115eafb71756110c61ec5f0e48c3
lomeliguillen@gmail.com <lomeliguillen@gmail.com>
2019-09-13 16:04:33 +0000

    Lesson 34

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   devise_for :users
+  resource :dashboard, only: [:show]
   root 'static_pages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :courses, only: [:index, :show] do
